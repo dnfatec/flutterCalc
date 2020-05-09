57bdfd52f9f5aeaefab0db2b357d099371c62e9a
dnfatec <danilo@fatecitapetininga.edu.br>
2020-05-09 10:26:58 -0300

    Alterado novamente

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo to git',
+      title: 'Flutter Demo to git again calc',
       theme: ThemeData(
         // This is the theme of your application.
         //
